d62e6e8e557beaae529b4814dbe8e9d2730c469f
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-10-16 12:00:37 -1000

    R template started

---
+++ apriori.R
@@ -0,0 +1,7 @@
+# Apriori
+
+# Data Preprocessing
+# install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
